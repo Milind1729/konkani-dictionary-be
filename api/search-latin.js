8769f111a6ba55a73a1312412f3c2625a0c438e3
Milind189 <milind.lawande@turtlemint.com>
2025-02-02 08:53:56 +0530

    Refactor

--- api/search-latin.js
+++ api/search-latin.js
@@ -7,10 +7,7 @@ dotenv.config();
 // MongoDB connection function
 const connectDb = async () => {
   if (mongoose.connections[0].readyState) return;
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 export default async (req, res) => {
